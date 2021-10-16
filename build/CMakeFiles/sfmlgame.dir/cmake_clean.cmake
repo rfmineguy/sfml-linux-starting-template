45bbf3534bf33aab9800a4e7dace27756e26400f
rfmineguy <rfmineguy@gmail.com>
2021-10-16 13:24:52 -0700

    This is my sfml game template

---
+++ build/CMakeFiles/sfmlgame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfmlgame.dir/main.cpp.o"
+  "CMakeFiles/sfmlgame.dir/main.cpp.o.d"
+  "sfmlgame"
+  "sfmlgame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfmlgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sfmlgame.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for sfmlgame.
+# This may be replaced when dependencies are built.
